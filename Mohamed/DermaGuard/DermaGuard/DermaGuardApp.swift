b8e6d75e3cfe7788b68aba0c6b5a97f639fbbe04
Mohamed El Bechir <122316239+medelbechir@users.noreply.github.com>
2023-06-30 02:16:08 +0200

    Add files via upload

---
+++ Mohamed/DermaGuard/DermaGuard/DermaGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DermaGuardApp.swift
+//  DermaGuard
+//
+//  Created by Med Elbechir on 26/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct DermaGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
